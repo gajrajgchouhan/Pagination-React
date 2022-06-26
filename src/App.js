59fe2e407fca103ee2e3ebc64fb41324eee100a7
Gajraj Singh Chouhan <gajraj@antino.io>
2022-06-26 09:46:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,84 +1,95 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 import { faker } from "@faker-js/faker";
+import { useRef } from "react";
 
 const Pagination = ({ currPage, setCurrPage, totalPages }) => {
-  return (
-    <div className="flex [&>*]:outline gap-4">
-      <button
-        className="disabled:opacity-40"
-        disabled={currPage === 1}
-        onClick={() => setCurrPage((curr) => curr - 1)}
-      >
-        Prev
-      </button>
-      <div>You are at page {currPage}</div>
-      <button
-        className="disabled:opacity-40"
-        disabled={currPage === totalPages}
-        onClick={() => setCurrPage((curr) => curr + 1)}
-      >
-        Next
-      </button>
-    </div>
-  );
+    return (
+        <div className="flex [&>*]:outline gap-4">
+            <button
+                className="disabled:opacity-40"
+                disabled={currPage === 1}
+                onClick={() => setCurrPage((curr) => curr - 1)}
+            >
+                Prev
+            </button>
+            <div>You are at page {currPage}</div>
+            <button
+                className="disabled:opacity-40"
+                disabled={currPage === totalPages}
+                onClick={() => setCurrPage((curr) => curr + 1)}
+            >
+                Next
+            </button>
+        </div>
+    );
 };
 
-const dataAtServer = [...Array(100)].map(() => faker.random.words(3));
+const dataAtServer = [...Array(100)].map(() => faker.random.word());
+console.log("all data", dataAtServer);
 
 const getAllDatafromServer = (page, length) => {
-  const skip = (page - 1) * length;
-  return {
-    data: dataAtServer.slice(skip + 1, skip + length),
-    totalPages: Math.ceil(dataAtServer.length / length)
-  };
+    const skip = (page - 1) * length;
+    return {
+        data: dataAtServer.slice(skip + 1, skip + length),
+        totalPages: Math.ceil(dataAtServer.length / length),
+    };
 };
 
 const searchfromServer = (page, length, query) => {
-  const skip = (page - 1) * length;
-  return {
-    data: dataAtServer
-      .filter((d) => d.contains(query))
-      .slice(skip + 1, skip + length),
-    totalPages: Math.ceil(dataAtServer.length / length)
-  };
+    const skip = (page - 1) * length;
+    return {
+        data: dataAtServer
+            .filter((d) => d.toLowerCase().includes(query.toLowerCase()))
+            .slice(skip + 1, skip + length),
+        totalPages: Math.ceil(dataAtServer.length / length),
+    };
 };
 
 export default function App() {
-  const [data, setData] = useState();
-  const [currPage, setCurrPage] = useState(1);
-  const [query, setQuery] = useState("");
+    const [data, setData] = useState();
+    const [currPage, setCurrPage] = useState(1);
+    const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    if (query !== "") {
-      // setData(() => {})
-    } else {
-      setData(getAllDatafromServer(currPage, 10));
-    }
-  }, [currPage]);
+    useEffect(() => {
+        if (query !== "") {
+            setData(() => searchfromServer(currPage, 10, query));
+        } else {
+            setData(() => getAllDatafromServer(currPage, 10));
+        }
+    }, [currPage, query]);
 
-  return (
-    <div className="flex flex-col gap-4 justify-center items-center mt-4">
-      <header className="flex gap-4">
-        <input
-          className="outline"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          type="text"
-        />
-        <button type="button" onClick={() => {}}>
-          Submit
-        </button>
-      </header>
-      <div>Data</div>
-      <div className="flex flex-col gap-4 [&>*]:outline">
-        {data?.data?.map((d) => (
-          <div>{d}</div>
-        ))}
-      </div>
-      <Pagination
-        {...{ currPage, setCurrPage, totalPages: data?.totalPages }}
-      />
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-4 justify-center items-center mt-4">
+            <header className="flex gap-4">
+                <input
+                    className="outline"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    type="text"
+                />
+                <button
+                    type="button"
+                    onClick={() => {
+                        setCurrPage(1);
+                    }}
+                >
+                    Submit
+                </button>
+            </header>
+            <div>Data</div>
+            <div className="flex flex-col gap-4 [&>*]:outline">
+                {data?.data?.map((d) => (
+                    <div>{d}</div>
+                ))}
+            </div>
+            <Pagination
+                {...{
+                    currPage: currPage,
+                    setCurrPage,
+                    totalPages: data?.totalPages,
+                }}
+            />
+        </div>
+    );
 }
